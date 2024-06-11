29a47b50436d6960a716bcc2c9265de97baa9b57
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-11 10:28:31 +0200

    added imports from pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
+//import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "../Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
+import "./index.css"
+
+import "./server"
 
 function App() {
-
   return (
-    <h1>Start here</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index="host" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
+
+  export default (App)
